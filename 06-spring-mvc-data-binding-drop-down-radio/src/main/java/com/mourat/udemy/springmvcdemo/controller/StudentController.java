81c3db47b7a7105d81b4fb067c76e76ac5917df8
Mourat Achmet oglou Iliaz <mayrit91@gmail.com>
2025-05-30 14:54:50 +0300

    Radio button implementation + read values from properties + file rename

--- 06-spring-mvc-data-binding-drop-down-radio/src/main/java/com/mourat/udemy/springmvcdemo/controller/StudentController.java
+++ 06-spring-mvc-data-binding-drop-down-radio/src/main/java/com/mourat/udemy/springmvcdemo/controller/StudentController.java
@@ -1,14 +1,22 @@
 package com.mourat.udemy.springmvcdemo.controller;
 
 import com.mourat.udemy.springmvcdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    List<String> countries;
+    @Value("${languages}")
+    List<String> languages;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model model){
@@ -19,13 +27,17 @@ public class StudentController {
         // add student to the model
         model.addAttribute("student", std);
 
+        model.addAttribute("countries", countries);
+
+        model.addAttribute("languages", languages);
+
         return "student-form";
     }
 
     @PostMapping("/processStudentForm")
     public String processForm(@ModelAttribute("student") Student student){
 
-        System.out.println("Student " + student.getFirstName() + " " + student.getLastName() + " from " + student.getCountry());
+        System.out.println("Student " + student.getFirstName() + " " + student.getLastName() + " from " + student.getCountry() + " with favorite language " + student.getFavoriteLanguage());
         return "student-confirmation";
     }
 }
