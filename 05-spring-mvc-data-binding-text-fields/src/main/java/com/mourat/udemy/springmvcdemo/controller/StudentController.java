d1d533c451a146950c4026c18def56d4b9f52c2a
Mourat Achmet oglou Iliaz <mayrit91@gmail.com>
2025-05-30 14:20:52 +0300

    Added mvc data binding text fields tutorial material

---
+++ 05-spring-mvc-data-binding-text-fields/src/main/java/com/mourat/udemy/springmvcdemo/controller/StudentController.java
@@ -0,0 +1,31 @@
+package com.mourat.udemy.springmvcdemo.controller;
+
+import com.mourat.udemy.springmvcdemo.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+
+    @GetMapping("/showStudentForm")
+    public String showForm(Model model){
+
+        // create a student object
+        Student std = new Student();
+
+        // add student to the model
+        model.addAttribute("student", std);
+
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student student){
+
+        System.out.println("Student " + student.getFirstName() + " " + student.getLastName());
+        return "student-confirmation";
+    }
+}
